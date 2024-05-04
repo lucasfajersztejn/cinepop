119471f7e0dffdb83a8a605d23cc9cda9575ea68
lucasfajersztejn <96478667+lucasfajersztejn@users.noreply.github.com>
2024-05-04 14:05:05 +0200

    feat: navbar responsive created and working

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,16 +1,26 @@
+import { useState } from "react";
 import "./App.css";
 import Hello from "./components/hello";
 import Footer from "./components/ui/footer/footer";
 import Navbar from "./components/ui/navbar/navbar";
 
 function App() {
+
+  const [darkMode, setDarkMode] = useState(false);
+
+  const handleDarkMode = () => {
+    setDarkMode(prev => !prev)
+  }
+
   return (
-    <>
-      <Navbar />
+    <main className={ darkMode ? 'dark' : '' }>
+      <header className="backdrop-blur-md bg-dark-200/30 fixed w-full z-20 top-0 left-0">
+        <Navbar onDarkMode={handleDarkMode} darkMode={darkMode} />
+      </header>
       <Hello />
 
       <Footer />
-    </>
+    </main>
   );
 }
 
--- web/src/components/hello.jsx
+++ web/src/components/hello.jsx
@@ -1,7 +1,7 @@
 function Hello() {
   return (
     <>
-      <div className="m-10 text-xl bg-teal-400">Hello world!</div>
+      <div className="mt-40 lg:mt-44 xl:mt-48 m-10 text-xl bg-teal-400 dark:bg-slate-300 dark:text-red-200">Hello world!</div>
     </>
   );
 }
