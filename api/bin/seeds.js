8948c8e9fd9a87a8ce8d74cc72749082e35f740c
lucasfajersztejn <96478667+lucasfajersztejn@users.noreply.github.com>
2024-05-06 00:33:09 +0200

    feat: more info in the movie model

--- api/bin/seeds.js
+++ api/bin/seeds.js
@@ -2,17 +2,11 @@ require("dotenv").config();
 require("../configs/db.config");
 const axios = require("axios");
 const Movie = require("../models/movie.model");
-const Cinema = require("../models/cinema.model")
+const Cinema = require("../models/cinema.model");
 
 const movies = require("../data/movies.json");
 const genreId = require("../data/genreId.json");
-const cinemas = require("../data/cinemas.json")
-
-function replaceGenreIds(movie) {
-  const genreNames = movie.genre_ids.map(id => genreId[id]);
-  return { ...movie, genre_ids: genreNames };
-}
-const moviesWithGenreNames = movies.map(movie => replaceGenreIds(movie));
+const cinemas = require("../data/cinemas.json");
 
 // Movie.create(moviesWithGenreNames)
 //   .then((movie) => console.debug(`${movie.length} movies created.`))
@@ -21,10 +15,17 @@ const moviesWithGenreNames = movies.map(movie => replaceGenreIds(movie));
 // Cinema.create(cinemas)
 //   .then((cinema) => console.debug(`${cinema.length} movies created.`))
 //   .catch((error) => console.error(error))
-/*
+
+// function replaceGenreIds(movie) {
+//   const genreNames = movie.genre_ids.map((id) => genreId[id]);
+//   return { ...movie, genre_ids: genreNames };
+// }
+
+// const moviesWithGenreNames = movies.map((movie) => replaceGenreIds(movie));
+
 async function fetchMoviesAndTrailers() {
-  // Llamada para obtener las películas en cartelera
-  const url = "https://api.themoviedb.org/3/movie/now_playing?language=es-ES&page=3";
+  const url =
+    "https://api.themoviedb.org/3/movie/now_playing?language=es-ES&page=3";
   const options = {
     headers: {
       accept: "application/json",
@@ -35,30 +36,100 @@ async function fetchMoviesAndTrailers() {
   try {
     const response = await axios.get(url, options);
     const data = response.data;
+    const moviesJson = data.results;
 
-    const movies = data.results;
-
-    // Array para almacenar las películas con la clave youtube_key
     const moviesWithTrailers = [];
 
-    // Recorrer cada película para obtener sus trailers
-    for (const movie of movies) {
-      // Llamada para obtener los videos de la película
+    for (const movie of moviesJson) {
       const videoUrl = `https://api.themoviedb.org/3/movie/${movie.id}/videos`;
-      const videoResponse = await axios.get(videoUrl, options);
-      const videoData = videoResponse.data;
+      const castUrl = `https://api.themoviedb.org/3/movie/${movie.id}/credits`;
+      const movieDetailUrl = `https://api.themoviedb.org/3/movie/${movie.id}`;
+      const certification = `https://api.themoviedb.org/3/movie/${movie.id}/release_dates`;
 
-      // Filtrar los videos para obtener solo los trailers
+      const [
+        videoResponse,
+        castResponse,
+        movieDetailResponse,
+        certificationsResponse,
+      ] = await Promise.all([
+        axios.get(videoUrl, options),
+        axios.get(castUrl, options),
+        axios.get(movieDetailUrl, options),
+        axios.get(certification, options),
+      ]);
+
+      const videoData = videoResponse.data;
+      const castData = castResponse.data;
+      const movieDetailData = movieDetailResponse.data;
+      const movieCertifications = certificationsResponse.data;
+
       const trailers = videoData.results.filter(
         (video) => video.type === "Trailer"
       );
 
-      // Si hay al menos un trailer, añadir la película al array
+      const genreNames = movie.genre_ids.map((id) => genreId[id]);
+
+      const simplifiedCastData = castData.cast.map((cast) => ({
+        name: cast.name,
+        original_name: cast.original_name,
+        character: cast.character,
+        profile_path: cast.profile_path,
+      }));
+
+      const directorFilter = castData.crew.find(
+        (cast) => cast.job === "Director"
+      );
+
+      const directorInfo = directorFilter
+        ? {
+            name: directorFilter.name,
+            original_name: directorFilter.original_name,
+            profile_path: directorFilter.profile_path,
+            job: directorFilter.job,
+          }
+        : null;
+
+      const movieCertificationResult = movieCertifications.results.filter(
+        (certification) => certification.iso_3166_1 === "ES"
+      );
+      //console.log(movieCertificationResult[0].release_dates[0].certification);
+      let movieCertified;
+      if (movieCertificationResult.length > 0) {
+        
+          if (movieCertificationResult[0].release_dates[0].certification !== "") {
+            movieCertified = movieCertificationResult[0].release_dates[0].certification
+          } else {
+            movieCertified = "";
+          }
+      } else {
+        movieCertified = "";
+      }
+
+      if (movie.overview === "") {
+        movie.overview = "-";
+      }
+
       if (trailers.length > 0) {
-        // Añadir la clave youtube_key a la película
         const movieWithTrailer = {
-          ...movie,
-          youtube_key: trailers[0].key, // Suponiendo que tomamos el primer trailer
+          adult: movie.adult,
+          backdrop_path: movie.backdrop_path,
+          genre_ids: genreNames,
+          idMovie: movie.id,
+          original_language: movie.original_language,
+          original_title: movie.original_title,
+          overview: movie.overview,
+          popularity: movie.popularity,
+          poster_path: movie.poster_path,
+          release_date: movie.release_date,
+          title: movie.title,
+          video: movie.video,
+          vote_average: movie.vote_average,
+          vote_count: movie.vote_count,
+          youtube_key: trailers[0].key,
+          cast: simplifiedCastData,
+          director: directorInfo,
+          runTime: movieDetailData.runtime,
+          certification: movieCertified,
         };
 
         moviesWithTrailers.push(movieWithTrailer);
@@ -72,13 +143,16 @@ async function fetchMoviesAndTrailers() {
   }
 }
 
-// Llamar a la función y obtener las películas con trailers
 fetchMoviesAndTrailers()
+  .then((moviesWithTrailers) => {
+    return Movie.create(moviesWithTrailers);
+  })
   .then((movies) => {
-    console.log("Películas con trailers:", movies);
+    console.debug(`${movies.length} movies created.`);
   })
   .catch((error) => {
-    console.error("Error al obtener películas con trailers:", error);
+    console.error(
+      "Error fetching movies and trailers or charging the database:",
+      error
+    );
   });
-
-*/
--- api/models/movie.model.js
+++ api/models/movie.model.js
@@ -57,6 +57,40 @@ const movieSchema = new Schema(
     youtube_key: {
       type: String,
     },
+    cast: [{
+      name: {
+        type: String,
+      },
+      original_name: {
+        type: String,
+      },
+      character: {
+        type: String,
+      },
+      profile_path: {
+        type: String,
+      },
+    }],
+    director: {
+      name: {
+        type: String,
+      },
+      original_name: {
+        type: String,
+      },
+      profile_path: {
+        type: String,
+      },
+      job: {
+        type: String,
+      },
+    },
+    runTime: {
+      type: Number,
+    },
+    certification: {
+      type: String
+    }
   },
   {
     timestamps: true,
@@ -81,40 +115,3 @@ movieSchema.virtual("timesheets", {
 
 const Movie = mongoose.model("Movie", movieSchema);
 module.exports = Movie;
-
-/*
-{
-  "adult": false,
-  "backdrop_path": "/4woSOUD0equAYzvwhWBHIJDCM88.jpg",
-  "genre_ids": [
-    28,
-    27,
-    53
-  ],
-  "idMovie": 1096197,
-  "original_language": "en",
-  "original_title": "No Way Up",
-  "overview": "Characters from different backgrounds are thrown together when the plane they're travelling on crashes into the Pacific Ocean. A nightmare fight for survival ensues with the air supply running out and dangers creeping in from all sides.",
-  "popularity": 2011.445,
-  "poster_path": "/hu40Uxp9WtpL34jv3zyWLb5zEVY.jpg",
-  "release_date": "2024-01-18",
-  "title": "No Way Up",
-  "video": false,
-  "vote_average": 6.503,
-  "vote_count": 486,
-  "key": "UJa1zUYegqo",
-}
-
-{
-      "iso_639_1": "en",
-      "iso_3166_1": "US",
-      "name": "Clip",
-      "key": "UJa1zUYegqo",
-      "site": "YouTube",
-      "size": 1080,
-      "type": "Clip",
-      "official": true,
-      "published_at": "2024-02-16T16:31:54.000Z",
-      "id": "65d40e001d356301631e68cb"
-    }
-*/
